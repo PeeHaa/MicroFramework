dbdcc0885f296e4c2bbf4b6f61f785d0ddf8bab4
PeeHaa <info@pieterhordijk.com>
2012-01-14 20:37:50 +0100

    Added autoloader class

---
+++ MicroFW/MFW/AutoLoader.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * AutoLoader class provides autoload functions for spl_autoload
+ *
+ * This autoloader class provides autoload functions for project models and library code
+ *
+ * PHP version 5.3
+ *
+ * @category   MicroFramework
+ * @package    Autoloader
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    Build 1.0.0
+ */
+
+/**
+ * AutoLoader class provides autoload functions for spl_autoload
+ *
+ * This autoloader class provides autoload functions for project models and library code
+ *
+ * @category   MicroFramework
+ * @package    Autoloader
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class MFW_AutoLoader
+{
+    /**
+     * Load project models
+     *
+     * @param string $class The name of the class
+     */
+    public function loadProject($class)
+    {
+        $filename = SITE_PATH . '/models/' . $class . '.php';
+
+        if (!is_readable($filename)) {
+            return false;
+        }
+
+        include($filename);
+    }
+
+    /**
+     * Load library classes
+     *
+     * @param string $class The name of the class
+     */
+    public function loadLibrary($class)
+    {
+        $path = explode('_', $class);
+
+        $filename = LIB_PATH . '/' . implode('/', $path) . '.php';
+
+        if (!is_readable($filename)) {
+            return false;
+        }
+
+        include($filename);
+    }
+}
