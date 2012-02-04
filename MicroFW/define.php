c29f0c6fa9e5331cb3ce8802ce92a60ed52f1385
PeeHaa <info@pieterhordijk.com>
2012-02-04 19:23:27 +0100

    Implemented library constants

---
+++ MicroFW/define.php
@@ -0,0 +1,19 @@
+<?php
+
+define('MFW_LIBRARY_VERSION', '1.0.0');
+
+define('MFW_OS_WINDOWS', 1);
+define('MFW_OS_LINUX', 2);
+define('MFW_OS_MAC', 4);
+
+define('N', "\n");
+define('R', "\r");
+define('RN', "\r\n");
+
+define('MFW_ENV_DEBUG', 1);
+define('MFW_ENV_PRODUCTION', 2);
+
+define('MFW_ACCESSLEVEL_ALL', 0);
+define('MFW_ACCESSLEVEL_GUEST', 25);
+define('MFW_ACCESSLEVEL_USER', 50);
+define('MFW_ACCESSLEVEL_SUPERUSER', 99);
